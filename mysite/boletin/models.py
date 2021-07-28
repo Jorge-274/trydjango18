edaf1f14b46d2c74d6470a8f6abaf4591967a244
Jorge <jorgecespedes274@gmail.com>
2021-07-28 10:03:44 -0400

    commit inicial

---
+++ mysite/boletin/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+from django.shortcuts import render
+
+
+class SignUp(models.Model):
+    email = models.EmailField()
+    full_name = models.CharField(max_length=120, blank=False, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __unicode__(self):
+        return self.email
+
+
